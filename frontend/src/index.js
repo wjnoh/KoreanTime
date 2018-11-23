4538d7a48fef8900381622e8759639676072a9f9
WOOJE <woojenoh@gmail.com>
2018-11-23 16:36:14 +0900

    Delete Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,21 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 import "./shared/index.scss";
 import "reset-css";
 
-// redux
-import { createStore } from "redux";
-import rootReducer from "./redux/modules";
-import { Provider } from "react-redux";
-
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(rootReducer, devTools);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
